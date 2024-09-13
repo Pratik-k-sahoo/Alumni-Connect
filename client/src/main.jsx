6a515b634ff7319aea5cbca8f6a23b9fc6775431
PRATIK KUMAR SAHOO <93589149+Pratik-k-sahoo@users.noreply.github.com>
2024-09-13 23:34:39 +0530

    Changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { Toaster } from "sonner";
 import { Provider } from "react-redux";
 import store from "./redux/storeConfig.js";
@@ -11,12 +11,10 @@ import { PersistGate } from "redux-persist/integration/react";
 const persistor = persistStore(store);
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<App />
-			</PersistGate>
-			<Toaster />
-		</Provider>
-	</StrictMode>
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
+		<Toaster />
+	</Provider>
 );
