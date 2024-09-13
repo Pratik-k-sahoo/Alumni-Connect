5d0ebf263e1f9acb1de552851c271d9eb2403f5b
PRATIK KUMAR SAHOO <93589149+Pratik-k-sahoo@users.noreply.github.com>
2024-09-13 22:36:09 +0530

    Backend uri configured

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import Cookies from "js-cookie";
 
-export const socket = io("/", {
+export const socket = io("https://vercel-hackerwar.vercel.app/", {
 	auth: {
 		token: Cookies.get("token"),
 	},
